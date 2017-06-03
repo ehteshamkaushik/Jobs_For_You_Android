240afa90f1dd49e44fc1e8eb2c6fa177919bfae4
ehteshamkaushik <1405071.meuh@ugrad.cse.buet.ac.bd>
2017-06-03 08:09:48 +0600

    commit

--- app/src/main/java/com/example/kaushik/jobs4u/MainActivity.java
+++ app/src/main/java/com/example/kaushik/jobs4u/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.kaushik.jobs4u;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import static android.support.v7.appcompat.R.styleable.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.add_resume);
+
+
+    }
+    public void showDatePickerDialog(android.view.View v) {
+        DatePickerFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
